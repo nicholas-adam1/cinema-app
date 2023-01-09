b0da695faf58af5293e27f8a4871493647407cae
Nick Adam <na4hx@virginia.edu>
2023-01-09 17:26:28 -0500

    creates header component skeleton and installs node-sass

---
+++ src/components/header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../../logo.svg';
+
+const Header = () => {
+  return (
+    <>
+      <div className='header-nav-wrapper'>
+        <div className='header-bar'></div>
+        <div className='header-navbar'>
+          <div className='header-image'>
+            <img src={logo} alt='' />
+          </div>
+          <div className='header-menu-toggle'>
+            <span className='bar'></span>
+            <span className='bar'></span>
+            <span className='bar'></span>
+          </div>
+          <ul className='header-nav'>
+            <li className='header-nav-item'> Now Playing </li>
+            <li className='header-nav-item'> New Movies </li>
+          </ul>
+          <input className='search-input' type='text' placeholder='Search for a movie'></input>
+        </div>
+      </div>
+    </>
+
+  );
+};
+
+export default Header;
