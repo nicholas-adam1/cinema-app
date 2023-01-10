5be008a99747f04654fb3d019dbfb7fa61c16234
Nick Adam <na4hx@virginia.edu>
2023-01-10 15:34:47 -0500

    adds paginate component and styling

--- src/components/content/MainContent.js
+++ src/components/content/MainContent.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainContent.scss';
 import Slideshow from './slide-show/Slideshow';
+import Paginate from './paginate/Paginate';
 
 const MainContent = () => {
   const images = [
@@ -14,12 +15,24 @@ const MainContent = () => {
       url: 'https://static-cse.canva.com/blob/562138/RightBackground4.jpg'
     }
   ];
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginate = (type) => {
+    if (type === 'prev' && currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
   return (
     <div className="main-content">
       <Slideshow images={images} auto={true} showArrows={true} />
       <div className="grid-movie-title">
         <div className="movieType">Now Playing</div>
-        <div className="paginate">Paginate</div>
+        <div className="paginate">
+          <Paginate paginate={paginate} currentPage={currentPage} totalPages={10} />
+        </div>
       </div>
       {/* display grid component */}
     </div>
